ed549e69c09e942f4c0c1f5e1de8f995ba12a488
David-Kasilia <davidkasilia001@gmail.com>
2023-06-24 18:09:44 +0300

    Created chatbot

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", async () => {
@@ -20,7 +20,5 @@ if ("serviceWorker" in navigator) {
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>,
 )
