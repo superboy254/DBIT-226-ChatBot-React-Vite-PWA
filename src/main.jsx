30a8416610d8acd44fdd298f4b54e64a47b89e41
David-Kasilia <davidkasilia001@gmail.com>
2023-06-24 17:03:04 +0300

    Created PWA BASHBOT

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", async () => {
+    try {
+      const registration = await navigator.serviceWorker.register(
+        "/dist/sw.js",
+        {
+          scope: "/dist/", // Set the same scope as defined in the manifest file
+        }
+      );
+      console.log("Service worker registered:", registration);
+    } catch (error) {
+      console.log("Service worker registration failed:", error);
+    }
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
